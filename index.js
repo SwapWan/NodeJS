b9a1e763bee7a4f659bc47173943d53165867bb1
Wanderson Matos <121003026+SwapWan@users.noreply.github.com>
2023-07-25 19:20:24 -0300

    melhorias

--- index.js
+++ index.js
@@ -1,17 +1,54 @@
-// $ npm install express --save
-// $ npm install nodemon -g
+// $ npm install --save express --save
+// $ npm install --save nodemon -g
+// $ npm install --save sequelize mysql2
+// $ npm install --save express-handlebars
+// $ npm install --save body-parser
 
-const express = require("express");
-const app = express();
+const express = require("express")
+const app = express()
+const handlebars = require('express-handlebars')
+const bodyParser = require('body-parser')
+const Cliente = require('./models/Cliente')
+
+// Config
+    // Template Engine
+    app.engine('handlebars', handlebars.engine({defaultLayout: 'main'}))
+    app.set('view engine','handlebars')
+    // Body Parser
+    app.use(bodyParser.urlencoded({extended:false}))
+    app.use(bodyParser.json())
+
+
+
+// ROTAS
 
 app.get("/", function(req, res){
-    res.send("Painel Principal");
+    Cliente.findAll().then(function(clientes){
+    res.render('home', {clientes:clientes})        
+    })
+  
 })
+
 
 app.get("/avaliar/:token", function(req, res){
     res.send("Avaliar "+req.params.token);
 })
 
-app.listen(8081, function(){
-    console.log("Servidor Online");
-});
+app.get("/formulario", function(req, res){
+    res.render('formulario');
+})
+
+app.post("/add", function(req, res){
+    Cliente.create({
+        nome: req.body.titulo,
+        endereco: req.body.conteudo
+    }).then(function(){
+        res.send("ok")
+    }).catch(function(){
+        res.send("OFF")
+    })
+})
+
+// STARTANDO SERVIDOR
+
+app.listen(8081);
